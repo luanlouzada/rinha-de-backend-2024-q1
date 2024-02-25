5c31589670ed8e0209398ae33c565f69bf4ea243
Luan Louzada <luanlouzadadev@gmail.com>
2024-02-25 03:14:30 -0300

    fix:1

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/luanlouzada/rinha-de-backend-2024-q1/internal/handler"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/luanlouzada/rinha-de-backend-2024-q1/internal/handler"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Post("/clientes/{ID}/transações", handler.HandleTransaction)
-	r.Get("/clientes/[id]/extrato", HandleFunc)
+	r.Get("/clientes/[id]/extrato")
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
